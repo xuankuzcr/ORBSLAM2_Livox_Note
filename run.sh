c12edfd0048565a27f22dc85553369ce206a1659
xuankuzcr <18846640555@163.com>
2021-04-20 10:27:14 +0800

    initial repo

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./Examples/RGB-D/rgbd_tum /home/chunran/ORB_SLAM2_detailed_comments/Vocabulary/ORBvoc.txt /media/chunran/Chunran/Dataset-SUSTech/zyw/Livox.yaml /media/chunran/Chunran/Dataset-SUSTech/zyw/sustech_rgbd /media/chunran/Chunran/Dataset-SUSTech/zyw/sustech_rgbd/associations.txt 
+
